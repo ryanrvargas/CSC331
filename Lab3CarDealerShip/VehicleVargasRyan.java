d6ac1e909a7081c320d58ae876803f2938dd1b0b
Ryan R Vargas <ryanr99@live.com>
2025-02-09 15:00:52 -0500

    Vic CLass updates
    Variables and constructor added

---
+++ Lab3CarDealerShip/VehicleVargasRyan.java
@@ -0,0 +1,20 @@
+public class VehicleVargasRyan {
+    private String vin;
+    private String make;
+    private String model;
+    private int year;
+    private String engineType;
+    private String transmission
+    private double cost;
+    private double milage;
+
+    public VehicleVargasRyan(String make, String model, int year, String engineType, String transmission, double cost, double milage) {
+        make = this.make;
+        model = this.model;
+        year = this.year;
+        engineType = this.engineType;
+        transmission = this.transmission;
+        cost = this.cost;
+        milage = this.milage;
+    }
+}
