cc4372c8a0d684ff535be848b8f262f3d339653d
Ryan R Vargas <ryanr99@live.com>
2025-02-12 15:19:58 -0500

    Test push

--- Lab3CarDealerShip/DealershipVargasRyan.java
+++ Lab3CarDealerShip/DealershipVargasRyan.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 
 public class DealershipVargasRyan {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         VehicleVargasRyan BMW = new VehicleVargasRyan();
+
 
         System.out.printf("Enter vehicle Vargas: %s", BMW.getVin());
     }
