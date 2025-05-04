afa03502bf891a981cf03d0b9c388f24886003f3
Ryan R Vargas <ryanr99@live.com>
2025-05-04 14:55:44 -0400

    drill class done

--- FinalExamPrac/DrillVargas.java
+++ FinalExamPrac/DrillVargas.java
@@ -1,4 +1,16 @@
 package FinalExamPrac;
 
 public class DrillVargas extends ToolVargasRyan{
+    private String name;
+    private double basePrice;
+
+    public DrillVargas(String name, double basePrice){
+        super(name, basePrice);
+    }
+
+    @Override
+    public void calculateRentalPrice(int days){
+        double totalPrice = this.basePrice * days;
+        System.out.println("Total Price: $" + totalPrice + 5);
+    }
 }
